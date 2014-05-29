9f48edba0b65739341b73cd470d0a5831ba95dfc
Prelang Builder <builder@prelang.com>
2014-05-29 23:45:10 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fabolo::Application.config.secret_key_base = 'fa79abc65b83f0948f74aad96ecb6b666a374570cc22cc967c248c02412352086fba93edd8f928746ab0f83c2d4c6c5b082af93dc5873ae27af9d7f6d10b3f7d'
